2418841a150016bd64923f45db7491e929fb0744
Francisco Miranda <franciscomirandaa@gmail.com>
2023-12-06 12:35:01 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/maroshii/sse-dummy
+
+go 1.21.0
---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	http.HandleFunc("/sse-test", eventsHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func eventsHandler(w http.ResponseWriter, r *http.Request) {
+	// Set headers for Server-Sent Events
+	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Cache-Control", "no-transform, no-cache")
+
+	// Create a channel to send data to the client
+	dataChannel := make(chan string)
+
+	// Close the channel when the connection is closed
+	defer close(dataChannel)
+
+	// Send dummy data to the client in a second loop
+	go func() {
+		for {
+			data := "data: " + time.Now().Format("15:04:05") + "\n\n"
+			dataChannel <- data
+			time.Sleep(1 * time.Second)
+		}
+	}()
+
+	// Continuously write data to the client
+	for {
+		select {
+		case data := <-dataChannel:
+			fmt.Fprint(w, data)
+			w.(http.Flusher).Flush()
+		case <-r.Context().Done():
+			return
+		}
+	}
+}
